83eb00ce0bdda54d0cbc1f3b698d3065ced24149
cheseuca <150506251+cheseuca@users.noreply.github.com>
2024-06-22 22:20:06 +0800

    Added new button to ask player for a new game

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 
 const showComputerChoice = document.querySelector("#computerChoice");
 const myHumanChoice = document.querySelector("#choice");
-const showChoice = document.querySelector("#playerChoice");
+const showPlayerChoice = document.querySelector("#playerChoice");
 const showHumanPoints = document.querySelector("#humanPoints");
 const showComputerPoints = document.querySelector("#computerPoints");
 const showFinalResult = document.querySelector("#finalResult");
+const showRoundResult = document.querySelector("#roundResult");
+const newRound = document.querySelector("#newRound");
+
+showComputerChoice.textContent = '';
+showPlayerChoice.textContent = '';
+showRoundResult.textContent = '';
+
+const askPlayer = document.createElement("h3");
+askPlayer.textContent = "Do you want to play again?";
+const newGame = document.createElement("button");
+newGame.textContent = "Yes";
+const endGame = document.createElement("button");
+endGame.textContent = "No";
+
+newRound.appendChild(askPlayer);
+newRound.appendChild(newGame);
+newRound.appendChild(endGame);
+
+askPlayer.style.display = "none";
+newGame.style.display = "none";
+endGame.style.display = "none";
+
+let humanScore = 0;
+showHumanPoints.textContent = `Player:  ${humanScore}`;
+let computerScore = 0;
+showComputerPoints.textContent = `Computer: ${computerScore}`;
+let round = 0;
 
 function getComputerChoice() {
     //Computer will choose a random number between 1 and 3
@@ -37,11 +64,19 @@ myHumanChoice.addEventListener("click", (event) => {
         break;
         default:
             myHumanChoice.value = null;
+        break;
     }
 
-    console.log(getHumanChoice());
-    showComputerChoice.textContent = getComputerChoice();
-    showChoice.textContent = getHumanChoice();
+    if (myHumanChoice.value) {
+        const computerChoice = getComputerChoice();
+        const playerChoice = getHumanChoice();
+
+        showComputerChoice.textContent = computerChoice;
+        showPlayerChoice.textContent = playerChoice;
+        showRoundResult.textContent = playRound(playerChoice, computerChoice);
+    }
+    playGame();
+    updateScores();
 });
 
 function getHumanChoice() {
@@ -49,83 +84,88 @@ function getHumanChoice() {
     let humanChoice = myHumanChoice.value;
 
     //if humanChoice is not rock, paper or scissor, it will prompt the user to enter a valid input
-    if (humanChoice === null) {
-        alert("Game ended, Thanks for playing!");
-        return;
-    }
-    else if (humanChoice.toLowerCase() == "rock" || humanChoice.toLowerCase() == "paper" || humanChoice.toLowerCase() == "scissor") {
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissor") {
         return humanChoice;
-    }
-    else {
-        alert("Invalid input");
-        return humanChoice;
-    }
+    }/*else{
+        alert("Error");
+        return; 
+    }*/
 }
 
 function playRound(humanChoice, computerChoice) {
     //humanChoice vs computerChoice logic flow
     if (humanChoice == computerChoice) {
+        round++;
         return "Tie";
     }
     else if (humanChoice == "rock" && computerChoice == "paper") {
+        round++;
+        computerScore++;
         return "Paper beats Rock, Computer wins!";
     }
     else if (humanChoice == "paper" && computerChoice == "scissor") {
+        round++;
+        computerScore++;
         return "Scissor beats Paper, Computer wins!";
     }
     else if (humanChoice == "scissor" && computerChoice == "rock") {
+        round++;
+        computerScore++;
         return "Rock beats Scissor, Computer wins!";
     }
     else if (humanChoice == "paper" && computerChoice == "rock") {
+        round++;
+        computerScore++;
         return "Paper beats Rock, User wins";
     }else if (humanChoice == "scissor" && computerChoice == "paper") {
+        round++;
+        humanScore++;
         return "Scissor beats Paper, User wins!";
     }else if (humanChoice == "rock" && computerChoice == "scissor") {
+        round++;
+        humanScore++
         return "Rock beats Scissor, User wins!";
     }
 }
 
 function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    let round = 1;
-
-    //playGame will have 5 rounds and this uses for loops
-    for(let i = 1; i <= 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        let result = playRound(humanChoice, computerChoice);
-        console.log(result);
-
-        //if the user wins, the humanScore will increment by one
-        if(result.includes("User wins")){
-            humanScore++;
-        }
-        //if the computer wins, the computerScore will increment by one
-        else if(result.includes("Computer wins")){
-            computerScore++;
+    //playGame will have 5 rounds
+    if(round === 5){
+        if(humanScore > computerScore){
+            showFinalResult.textContent = "User wins the game!";
+        }else if(computerScore > humanScore){
+            showFinalResult.textContent = "Computer wins the game!";
+        }else if(humanScore == computerScore){
+            showFinalResult.textContent = "It's a tie!";
         }
 
-        //After the 5th round, it will announce who is the winner and end the game
-        if(round === 5){
-            if(humanScore > computerScore){
-                alert("User wins the game!");
-            }else if(computerScore > humanScore){
-                alert("Computer wins the game!");
-            }else{
-                alert("It's a tie!");
-            }
+        askPlayer.style.display = "block";
+        newGame.style.display = "block";
+        endGame.style.display = "block";
 
-            //playGame will ask the user if they want to play again and if they do, it will call playGame again
-            let playAgain = confirm("Do you want to play again?");
-            if (playAgain) { 
-                playGame(); 
-            } else { 
-                alert("Thanks for playing!"); 
-            }
-        }
-        round++;
+        //This will ask the user if they want to play again and if they do, it will call playGame again
+        newGame.addEventListener("click", () => {
+            resetGame();
+            playGame();
+        },{once: true});
+        endGame.addEventListener("click", () => {
+            newRound.textContent = "Thanks for Playing!";
+        },{once: true});
     }
 }
 
-playGame();
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    updateScores();
+    showComputerChoice.textContent = '';
+    showPlayerChoice.textContent = '';
+    showRoundResult.textContent = '';
+}
+
+function updateScores() {
+    showHumanPoints.textContent = `Player: ${humanScore}`;
+    showComputerPoints.textContent = `Computer: ${computerScore}`;
+}
+updateScores();
